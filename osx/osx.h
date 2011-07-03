9f5d3538557e0eaddb6d11f922c96428cdf8c8e3
Andrea Bolognani <eof@kiyuko.org>
2011-07-03 23:31:16 +0200

    Imported Upstream version 0.9.31

--- osx/osx.h
+++ osx/osx.h
@@ -1,3 +1,7 @@
-/* $scrotwm: osx.h,v 1.1 2009/11/25 16:12:13 marco Exp $ */
+/* $scrotwm: osx.h,v 1.2 2011/06/15 00:49:07 marco Exp $ */
 
 long long strtonum(const char *, long long, long long, const char **);
+
+#ifndef TAILQ_END
+#define	TAILQ_END(head)			NULL
+#endif
